910dc08fc938ed0515bb72033e9c31102f389db4
Shubham Kumar <kumarshubham417@gmail.com>
2023-04-26 00:20:46 +0530

    Update userController.js

--- controlers/userController.js
+++ controlers/userController.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const getUsers = async (req, res) => {
   try {
     let query = req.query;
-    let users = await UserModel.find(query);
+    let users = await UserModel.find();
     res.status(200).json({ data: users });
   } catch (err) {
     console.log(err);
