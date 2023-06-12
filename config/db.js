1011276f62d6d037cb543c9902eaad4998807065
Shubham Kumar <kumarshubham417@gmail.com>
2023-06-12 21:10:17 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.MONGODB_URL);
+const connection = mongoose.connect(process.env.MONGODB_URL,{
+  connectTimeoutMS: 30000,
+});
 
 module.exports = { connection };
